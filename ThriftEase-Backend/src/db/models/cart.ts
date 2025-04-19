6ae2f53f78b2408a850d6705ab65513d54e8d5bc
Oderinde Michael <Olubiyioderinde7@gmail.com>
2025-04-19 20:03:34 +0100

    feat: Add ownerId attribute to Cart model and update its initialization

--- ThriftEase-Backend/src/db/models/cart.ts
+++ ThriftEase-Backend/src/db/models/cart.ts
@@ -1,8 +1,10 @@
 import { DataTypes, Model, Optional } from "sequelize";
 import dbConnect from "../config/dbConnect";
+import { User } from "./user";
 
 interface CartAttributes {
   id: number;
+  ownerId: number;
 }
 
 interface CartCreationAttributes extends Optional<CartAttributes, "id"> {}
@@ -12,6 +14,7 @@ class Cart
   implements CartAttributes
 {
   declare id: number;
+  declare ownerId: number;
 }
 
 Cart.init(
@@ -22,10 +25,18 @@ Cart.init(
       autoIncrement: true,
       primaryKey: true,
     },
+    ownerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: User,
+        key: "id",
+      },
+    },
   },
   {
     sequelize: dbConnect,
     freezeTableName: true,
-    tableName: "Cart"
+    tableName: "Cart",
   }
 );
